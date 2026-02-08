3b9174651b23d5a67596a8a137260b4e621c33d6
thecrow1304 <fjahnel@gmx.de>
2026-02-08 14:40:20 +0100

    Update binary_sensor.py

--- custom_components/mqtt_dynamic_devices/binary_sensor.py
+++ custom_components/mqtt_dynamic_devices/binary_sensor.py
@@ -3,9 +3,13 @@ from homeassistant.components.binary_sensor import BinarySensorEntity
 class DynamicBinarySensor(BinarySensorEntity):
     def __init__(self, definition):
         self._attr_name = definition["name"]
-        self._attr_unique_id = definition["unique_id"]
-        self._state = definition["state"]
+        self._attr_unique_id = definition["entity_id"]
+        self._state = False
 
     @property
     def is_on(self):
         return self._state
+
+    def set_state(self, value):
+        self._state = bool(value)
+        self.async_write_ha_state()
