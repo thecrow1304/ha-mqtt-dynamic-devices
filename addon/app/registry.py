f48f9140af09d66248c62c191ff1819ede47357e
thecrow1304 <fjahnel@gmx.de>
2026-02-08 14:47:48 +0100

    Update registry.py

--- addon/app/registry.py
+++ addon/app/registry.py
@@ -1,39 +1,13 @@
-from mapper import map_entity
-
-class Registry:
-    def __init__(self):
-        self.devices = {}
-
-    def process(self, sensor, message):
-        device_id = sensor.get("deviceId")
-        if not device_id:
-            return
-
-        if device_id not in self.devices:
-            self.devices[device_id] = sensor
-            self.publish_device(sensor)
-
-        for key, value in message.items():
-            entity = map_entity(device_id, key, value)
-            if entity:
-                self.publish_entity(entity)
-
-    def publish_device(self, sensor):
-        print("Neues Gerät:", sensor.get("alias"))
-
-    def publish_entity(self, entity):
-        print("Entity Update:", entity)
-
-registry = Registry()
-
 from config_store import config_store
+from mapper import map_entity
+import json
 
 class Registry:
     def __init__(self):
         self.devices = {}
         self.last_messages = {}
 
-    def process(self, sensor, message):
+     def process(self, sensor, message):
         device_id = sensor.get("deviceId")
         if not device_id:
             return
@@ -41,6 +15,15 @@ class Registry:
         self.devices[device_id] = sensor
         self.last_messages[device_id] = message
 
-        # hier später: nur gemappte Felder weiterreichen
+        for key, value in message.items():
+            if not config_store.is_enabled(device_id, key):
+                continue
+
+            mapping = config_store.get_mapping(device_id, key)
+            entity = map_entity(device_id, key, value, mapping)
+
+            if entity:
+                self.publish_entity(entity)
+                self.publish_state(entity)
 
 registry = Registry()
