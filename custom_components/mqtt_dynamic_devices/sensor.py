e52d2aaed27f0cdd6e57efafa404fd5fe8003895
thecrow1304 <fjahnel@gmx.de>
2026-02-08 14:40:04 +0100

    Update sensor.py

--- custom_components/mqtt_dynamic_devices/sensor.py
+++ custom_components/mqtt_dynamic_devices/sensor.py
@@ -1,11 +1,17 @@
 from homeassistant.components.sensor import SensorEntity
+from homeassistant.helpers.entity_platform import async_add_entities
 
 class DynamicSensor(SensorEntity):
     def __init__(self, definition):
         self._attr_name = definition["name"]
-        self._attr_unique_id = definition["unique_id"]
-        self._state = definition["state"]
+        self._attr_unique_id = definition["entity_id"]
+        self._attr_native_unit_of_measurement = definition.get("unit")
+        self._state = None
 
     @property
     def native_value(self):
         return self._state
+
+    def set_state(self, value):
+        self._state = value
+        self.async_write_ha_state()
